f4a177cc58986608b9fb645b8dca9d026831bdd1
Nikelson Exantus <nikelsone1981@hotmail.com>
2019-12-05 23:08:48 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,15 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(dwarves) # code an argument here
+  if dwarves == 0
+    " "
+    else 
+      cartoon = []
+      dwarf.collect do |array, i|
+      cartoon << "#{i + 1}. #{array}"
   # Your code here
-end
+      end
+      puts "#{carton.join(" ")}"
+  end
+end 
 
 def summon_captain_planet# code an argument here
   # Your code here
@@ -10,7 +19,7 @@ def long_planeteer_calls# code an argument here
   # Your code here
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(cheese_types) # code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end

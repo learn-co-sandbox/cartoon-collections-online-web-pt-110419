8abadd56f549a766478ac6e87bc6b036f3e7dfe3
Nikelson Exantus <nikelsone1981@hotmail.com>
2019-12-06 00:27:23 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -34,5 +34,5 @@ end
 def find_the_cheese(strings) # code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  
+  strings.find do |cheese|
 end
